4d14bfb0580f8f0e54c111856352204888b94b97
Preston Bernstein <bernsteinpreston@gmail.com>
2024-03-06 00:26:53 -0500

    push up

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@nuxtjs/critters',
+    '@nuxtjs/fontaine',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/web-vitals',
+    'nuxt-purgecss',
+  ],
+  purgecss: {
+    enabled: true, // Always enable purgecss
+  },
+  critters: {
+    config: {
+      preload: 'swap'
+    }
+  },
+  fontMetrics: {
+    fonts: ['Inter', { family: 'Some Custom Font', src: '/fonts/Inter-VariableFont_slnt,wght.ttf'}]
+  },
+  css: ['/assets/css/main.css'],
+  ssr: true,
+  experimental: {
+    payloadExtraction: false,
+  },
+  router: {
+    options: {
+      strict: false,
+    },
+  },
+  sourcemap:false,
   devtools: { enabled: true }
 })
