2ad813882a02cfbe2c5e05008ac16263369592e1
Preston Bernstein <bernsteinpreston@gmail.com>
2024-03-11 13:57:23 -0400

    more work

---
+++ components/header/siteLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <a
+      :href="href"
+      target="_blank"
+      rel="noopener noreferrer"
+      class="text-xs leading-tight uppercase ml-4 transition-colors"
+      data-js="menu-button"
+    >
+      {{ text }}
+    </a>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      href: String,
+      text: String,
+    },
+  };
+  </script>
--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,7 +1,18 @@
 <template>
-    <header class="z-50 fixed top-0 left-0 right-0 pt-4 pb-4 grid grid-cols-2 pl-2 pr-2 justify-center gap-2">
-        <div class="grid grid-cols-2 justify-between items-center py-2 relative bg-white border border-gray-300 rounded-md" data-js="header">
-            <p>Header Content</p>
+    <header
+        class="z-50 fixed top-0 left-0 right-0 pt-4 pb-4 grid grid-cols-2 pl-2 pr-2 justify-center gap-2"
+    >
+        <div
+            class="grid grid-cols-2 justify-between items-center py-2 relative bg-white border border-gray-300 rounded-md"
+            data-js="header"
+        >
+            <HeaderSiteLink
+                href="https://prestonbernstein.com"
+                text="Preston Bernstein"
+            />
+            <button class="text-x leading-tight border-0 py-1 px-2 m-0 mr-1 transition-colors  text-center rounded-sm cursor-pointer uppercase">
+                Menu
+              </button>            
         </div>
     </header>
 </template>
