5dfaaa626b5bc57272469a33147c293e6f14bdaa
Preston Bernstein <bernsteinpreston@gmail.com>
2024-03-11 15:21:43 -0400

    little bit more

--- components/header/menuButton.vue
+++ components/header/menuButton.vue
@@ -23,7 +23,4 @@ const toggleMenu = () => store.toggleMenu();
   <button @click="toggleMenu" class="text-xs leading-tight border-0 py-1 px-2 m-0 mr-1 transition-colors text-center rounded-sm cursor-pointer uppercase">
     {{ store.isMenuOpen ? props.text.closed : props.text.open }}
   </button>
-  <nav v-if="store.isMenuOpen" class="navigation-menu">
-    <!-- Navigation items here -->
-  </nav>
 </template>
--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,9 +1,9 @@
 <template>
     <header
-        class="z-50 fixed top-0 left-0 right-0 pt-4 pb-4 grid grid-cols-2 pl-2 pr-2 justify-center gap-2"
+        class="z-50 fixed top-0 left-0 right-0 pt-4 pb-4 pl-2 pr-2 justify-center gap-2"
     >
         <div
-            class="grid grid-cols-2 justify-between items-center py-2 relative bg-white border border-gray-300 rounded-md"
+            class="justify-between items-center py-2  grid grid-cols-3 relative bg-white border border-gray-300 rounded-md"
             data-js="header"
         >
             <HeaderSiteLink
@@ -11,6 +11,7 @@
                 text="Preston Bernstein"
             />
             <HeaderMenuButton :text="{ open: 'Menu', closed: 'Close' }"/>
+            <HeaderLogo />
         </div>
     </header>
 </template>
